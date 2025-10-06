d775c1e6dd66f1e43c7fd2e0dc6481cf616a05a5
deadr1pley <vanmatrix@hotmail.com>
2025-10-06 13:48:18 +0200

    uppdated the code to corret, were missing $ in the end of first const

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_URL}?api_key={API_KEY}`;
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
