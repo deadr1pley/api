d512096dba6db0d6bb75654e33b356c5637de409
deadr1pley <vanmatrix@hotmail.com>
2025-10-06 16:30:44 +0200

    uppdated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,21 +38,24 @@ async function postForm(e) {
      if(response.ok) {
         displayErrors(data);
      } else {
+        displayException(data);
         throw new Error(data.error);
      }
 }
 
 function displayErrors(data) {
 
+    let results = "";
+
     let heading = `JSHint Results for ${data.file}`;
 
     if (data.total_errors === 0) {
         results = `<div class="no_errors">No errors reported!</div>`;
     } else {
-        results = `<div>Total Errors: <span class="error_count">${data.total_errors}>/span></div>`;
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
         for (let error of data.error_list) {
             results += `<div>At line <span class="line">${error.line}</span>, `;
-            results += `column <span class="column">${error.col}</span></div>`;
+            results += `column <span class="column">${error.col}:</span></div>`;
             results += `<div class="error">${error.error}</div>`;
         }
     }
@@ -72,6 +75,7 @@ async function getStatus(e) {
     if (response.ok) {
        displayStatus(data); 
     } else{
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -81,6 +85,20 @@ function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
